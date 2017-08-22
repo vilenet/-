5dc9dc240e23d205dbc10bab2dc0db9ead69d6d4
vilenet <vilenet607@gmail.com>
2017-08-22 20:16:07 +0300

    Добавил первое решение

---
+++ 3.1.cpp
@@ -0,0 +1,38 @@
+/*************************************************************************
+ 1. Найти сумму отрицательных элементов массива. 
+ 2. Найти произведение элементов массива, расположенных между максимальным  
+ и минимальным элементами.
+ 3. Упорядочить элементы массива по возрастанию. 
+*************************************************************************/
+
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main()
+{ setlocale(LC_ALL, "Russian");
+
+    const int N = 10;
+    int a[N] = {1, 3, -5, 1, -2, 1, -1, 3, 8, 4};
+    int i, imax, imin, count;
+
+    for(i = imax = imin = 0; i < N; ++i){
+       if(a[i] > a[imax]) imax = i;
+       if(a[i] < a[imin]) imin = i;
+    }
+
+    cout << "\n\t max= " << a[imax] << " min= " << a[imin];
+    int ibeg = imax < imin ? imax : imin;
+    int iend = imax < imin ? imin : imax;
+    cout << "\n\t ibeg= " << ibeg << " iend= " << iend <<endl;;
+
+    for(int j = imin; j < imax; ++j)
+        cout << a[j] << " ";
+    cout<<endl;
+
+    for(count = 0, i = ibeg + 1; i < iend; ++i)
+       if(a[i] > 0) count++;
+
+    cout<<" Количество положительных: " << count << endl;
+}
+
