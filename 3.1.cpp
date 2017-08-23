90bad9b78240dadb37c516b255fa112f6bd836b8
vilenet <vilenet607@gmail.com>
2017-08-23 12:18:35 +0300

    добавлено 3.2.cpp

--- 3.1.cpp
+++ 3.1.cpp
@@ -1,38 +1,48 @@
 /*************************************************************************
  1. Найти сумму отрицательных элементов массива.
- 2. Найти произведение элементов массива, расположенных между максимальным 
+ 2. Найти произведение элементов массива, расположенных между максимальным
     и минимальным элементами.
  3. Упорядочить элементы массива по возрастанию.
 *************************************************************************/
 
 #include <iostream>
-#include <locale>
 using namespace std;
 
 int main()
-{ setlocale(LC_ALL, "Russian");
-
+{
     const int N = 10;
-    int a[N] = {1, 3, -5, 1, -2, 1, -1, 3, 8, 4};
-    int i, imax, imin, count;
+    int a[N] = {1, 3, -5, 2, 2, 2, 2, 2, 8, -4};
+    int imax = 0, imin = 0, negSum = 0, productSum = 0;
 
-    for(i = imax = imin = 0; i < N; ++i){
+    for(int i=0; i < N; ++i){
+       //1 Находим сумму отрицательных элементов
+       if(a[i] < 0) negSum += a[i];
+
+       // Находим расположение(индексы) мин и макс элементов
        if(a[i] > a[imax]) imax = i;
        if(a[i] < a[imin]) imin = i;
     }
 
-    cout << "\n\t max= " << a[imax] << " min= " << a[imin];
-    int ibeg = imax < imin ? imax : imin;
-    int iend = imax < imin ? imin : imax;
-    cout << "\n\t ibeg= " << ibeg << " iend= " << iend <<endl;;
+    //2 Находим произведение между мин и макс элементами
+    productSum = a[imin+1];
+    for(int j = imin+1; j < imax-1; ++j)
+       productSum *= a[j+1];
 
-    for(int j = imin; j < imax; ++j)
-        cout << a[j] << " ";
-    cout<<endl;
+    //3 Sort array
+    for( int i = 0; i < N - 1; ++i )
+       for( int j = N - 1; j > 0; --j)
+          if( a[j - 1] > a[j] ){
+             int temp = a[j - 1];
+             a[j - 1] = a[j];
+             a[j] = temp;
+          }
 
-    for(count = 0, i = ibeg + 1; i < iend; ++i)
-       if(a[i] > 0) count++;
+    cout<<"1 Sum of negative elements: " << negSum << endl;
+    cout<<"2 The product of elements between the min and max element: "<<productSum<<endl;
+    cout<<"3 Sorting array: ";
+    for( int i = 0; i < N; ++i )
+        cout << a[i] << " ";
 
-    cout<<"Количество положительных: " << count << endl;
+    cout << endl;
 }
 
---
+++ 3.2.cpp
@@ -0,0 +1,54 @@
+/**********************************************************************
+ 1. Найти сумму положительных элементов массива.
+ 2. Найти произведение элеентов массива, расположенных между
+    максимальным по модулю и минимальным по модулю элементами.
+ 3. Упорядочить элементы по убыванию.
+**********************************************************************/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+   const int N = 10;
+   int a[N] = {-3, 1, -5, 2, 2, 2, 2, 2, 8, -4};
+   int posSum = 0, productSum = 1;
+   int maxElem = a[0];
+   int minI = 0, maxI = 0;
+
+   //1 Находим сумму положительных элементов
+   for(int i=0; i < N; ++i)
+       if(a[i] > 0) posSum += a[i];
+
+    // Находим расположение(индексы) мин по модулю и макс по модулю элементов
+    for( int i = 1; i < N; ++i){
+        if( abs( a[i] ) > maxElem){
+            maxElem = a[i];
+            maxI = i;
+        }
+        if(abs(a[i]) < abs(a[minI]))
+            minI = i;
+    }
+
+    //2 Произведение элеентов, расположенных между макс и мин по модулю элементами.
+    for( int i = minI + 1; i < maxI ; ++i)
+      productSum *= a[i];
+
+    //3 Сортируем массив по убыванию
+    for( int i = N; i > 0; --i )
+       for( int j = 0; j < N; ++j)
+          if( a[j + 1] > a[j] ){
+             int temp = a[j + 1];
+             a[j + 1] = a[j];
+             a[j] = temp;
+          }
+
+    cout<<"1 Sum of positive elements: " << posSum << endl;
+    cout<<"2 The product of elements between the min and max element: "<<productSum<<endl;
+    cout<<"3 Sorting array: ";
+    for( int i = 0; i < N; ++i )
+        cout << a[i] << " ";
+
+    cout << endl;
+}
